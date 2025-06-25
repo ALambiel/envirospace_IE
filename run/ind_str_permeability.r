1bd31cbcbe3c277822d75d70b0f596303a3a4383
ALambiel <audrey.lambiel@unige.ch>
2025-06-25 17:34:33 +0200

    Update ind_str_permeability.r

--- run/ind_str_permeability.r
+++ run/ind_str_permeability.r
@@ -51,7 +51,7 @@
     shared_directory <- "path/to/the/root/of/the/shared/folder"
     # Specify the name of an existing project or choose your new project's name
     # Please note that if you enter an existing project name, previously calculated results for this indicator may be overwritten.
-    project_name <- "version name"
+    version <- "version name"
     # Name of the pillar
     pillar_name <- "STRUCTURE" 
     # Name of the indicator
@@ -107,7 +107,7 @@ session_path <- strsplit(session_path, '/')[[1]]
 # Isolate the name of the script
 script_name <- tools::file_path_sans_ext(tail(session_path, n=1))
 # Set the name of error file
-error_file <- paste0(project_name, "_", script_name, "_err.out")
+error_file <- paste0(version, "_", script_name, "_err.out")
 # Create it
 err <- file(error_file, open = "a")
 
@@ -218,11 +218,11 @@ tryCatch({
     # -----------------------------------------------------------------------------------
     # 1.3) Working directories ----------------------------------------------------------
         # Path to the working directory
-        work_directory <- file.path(shared_directory, "OUTPUTS", "INDICATORS", project_name)
+        work_directory <- file.path(shared_directory, "OUTPUTS","INDICATORS")
         # Folder for the many intermediate results, which can be deleted at the end
-        scratch_folder <- file.path(work_directory, "scratch", paste(pillar_name, indicator_name, date, sep = "_"))
+        scratch_folder <- file.path(work_directory, pillar_name, indicator_name, version, "scratch")
         # Directory for final outputs
-        output_folder <- file.path(work_directory, pillar_name, indicator_name)
+        output_folder <- file.path(work_directory, pillar_name, indicator_name, version)
     
         # Create the project folder and all its subfolders (if needed)
         dir.create(work_directory, showWarnings = FALSE, recursive = TRUE)
@@ -230,10 +230,10 @@ tryCatch({
         dir.create(output_folder, showWarnings = FALSE, recursive = TRUE)
     
         # Initialising the tracking file 
-        tracking_file <- paste0(project_name, "_", script_name, ".txt")
+        tracking_file <- paste0(version, "_", script_name, ".txt")
         writeLines(paste(Sys.time(),"RUNNING ..."), con=tracking_file)
         # Initialising the metadata text file
-        info <- c(paste0("Version name : ", project_name, "\n",
+        info <- c(paste0("Version name : ", version, "\n",
                          "Date : ", date, "\n",
                          "User : ", user, "\n\n",
                          pillar_name, " - ", indicator_name, "\n",
@@ -275,7 +275,7 @@ tryCatch({
                                " * Study area: ", study_area_path, "\n",
                                " * Natural habitat map: ", habitat_map_path, "\n",
                                " * Attribute table: ", mn_attribute_table_path, "\n",
-                               " * Reading time: ", time_loading, " min", "\n\n"))
+                               " * Loading time: ", time_loading, " min", "\n\n"))
     # -----------------------------------------------------------------------------------
     # 2) Compute permeability -----------------------------------------------------------
     # 2.1) Assigning permeability values ------------------------------------------------
