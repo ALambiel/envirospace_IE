526b6f00e2c6660a6a289e4cc6e54a4f4169f50d
ALambiel <audrey.lambiel@unige.ch>
2025-06-25 17:22:41 +0200

    Update ind_compo_habnat.r

--- run/ind_compo_habnat.r
+++ run/ind_compo_habnat.r
@@ -54,7 +54,7 @@
     shared_directory <- "path/to/the/root/of/the/shared/folder"
     # Specify the name of an existing project or choose your new project's name
     # Please note that if you enter an existing project name, previously calculated results for this indicator may be overwritten.  
-    project_name <- "version name" 
+    version <- "version name" 
     # Name of the pillar 
     pillar_name <- "COMPOSITION" 
     # Name of the indicator
@@ -102,7 +102,7 @@ session_path <- strsplit(session_path, '/')[[1]]
 # Isolate the name of the script
 script_name <- tools::file_path_sans_ext(tail(session_path, n=1))
 # Set the name of error file
-error_file <- paste0(project_name, "_", script_name, "_err.out")
+error_file <- paste0(version, "_", script_name, "_err.out")
 # Create it
 err <- file(error_file, open = "a")
 
@@ -195,11 +195,11 @@ tryCatch({
     # -----------------------------------------------------------------------------------
     # 1.3) Working directories ----------------------------------------------------------
         # Path to the working directory
-        work_directory <- file.path(shared_directory, "OUTPUTS","INDICATORS", project_name) 
+        work_directory <- file.path(shared_directory, "OUTPUTS","INDICATORS")
         # Folder for the many intermediate results, which can be deleted at the end
-        scratch_folder <- file.path(work_directory, "scratch", paste(pillar_name, indicator_name, date, sep="_"))
+        scratch_folder <- file.path(work_directory, pillar_name, indicator_name, version, "scratch")
         # Directory for final outputs
-        output_folder <- file.path(work_directory, pillar_name, indicator_name)
+        output_folder <- file.path(work_directory, pillar_name, indicator_name, version)
         # Subfolder for 'Red List' habitat (LR)
         LR <- file.path(output_folder, "LR") 
         # Subfolder for 'Non Red List' habitat (NLR)
@@ -213,10 +213,10 @@ tryCatch({
         dir.create(NLR, showWarnings = FALSE, recursive = TRUE)
             
         # Initialising the tracking file 
-        tracking_file <- paste0(project_name, "_", script_name, ".txt")
+        tracking_file <- paste0(version, "_", script_name, ".txt")
         writeLines(paste(Sys.time(), "RUNNING ..."), con=tracking_file)
         # Initialising the metadata text file
-        info <- c(paste0("Version : ", project_name, "\n",
+        info <- c(paste0("Version : ", version, "\n",
                          "Date : ", date, "\n",
                          "User : ", user, "\n\n",
                          pillar_name, " - ", indicator_name, "\n",
@@ -267,7 +267,7 @@ tryCatch({
                                        " * Correspondence table: ", corr_table_path, "\n",
                                        " * Categories not included: ","\n"))
         info <- c(info, paste0("  '", excluded_categories, "'","\n"))
-        info <- c(info, paste0(" * Reading time : ", time_loading, " min", "\n\n"))
+        info <- c(info, paste0(" * Loading time : ", time_loading, " min", "\n\n"))
     # -----------------------------------------------------------------------------------
     # 2) Computing ----------------------------------------------------------------------
     # 2.1) Reclassification of habitat map categories -----------------------------------
